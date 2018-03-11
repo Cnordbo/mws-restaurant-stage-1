0de41417d8ea2bd7aa7d5c34bdce441a11276673
Christer Nordbø <cnordbo@gmail.com>
2018-03-11 11:05:26 +0100

    Remove aria tags on map, use HTML5 address on address field

--- js/main.js
+++ js/main.js
@@ -152,11 +152,12 @@ createRestaurantHTML = (restaurant) => {
   neighborhood.innerHTML = restaurant.neighborhood;
   li.append(neighborhood);
 
-  const address = document.createElement('p');
+  const address = document.createElement('address');
   address.innerHTML = restaurant.address;
   li.append(address);
 
   const more = document.createElement('a');
+  more.setAttribute('aria-label','View details about ' + restaurant.name);
   more.innerHTML = 'View Details';
   more.href = DBHelper.urlForRestaurant(restaurant);
   li.append(more)
